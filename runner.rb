df1bbc916fdb0f253024a5831a4692611c96a38c
Kensuke Nagae <kyanny@gmail.com>
2014-01-19 00:00:22 +0900

    [wip]

--- runner.rb
+++ runner.rb
@@ -6,4 +6,4 @@ exit if payload.action != "opened"
 
 compare_linker = CompareLinker.new(payload.repo_full_name, payload.pr_number)
 compare_links = compare_linker.make_compare_link.join("\n")
-puts compare_linker.add_comment(repo_full_name, pr_number, compare_links)
+puts compare_linker.add_comment(payload.repo_full_name, payload.pr_number, compare_links)
