32de3761f1345278046971fa7aa66e7d81f310af
Kensuke Nagae <kyanny@gmail.com>
2014-01-20 03:13:48 +0900

    debug

--- lib/compare_linker/rack_app.rb
+++ lib/compare_linker/rack_app.rb
@@ -11,12 +11,14 @@ class CompareLinker
     end
 
     post "/webhook" do
+      require 'pp'
       payload = CompareLinker::WebhookPayload.new(params["payload"])
+      pp payload
       if payload.action == "opened"
         compare_linker = CompareLinker.new(payload.repo_full_name, payload.pr_number)
         compare_linker.formatter = CompareLinker::Formatter::Markdown.new
         compare_links = compare_linker.make_compare_links.join("\n")
-        require 'pp'
+        pp compare_links
         pp [payload.repo_full_name, payload.pr_number, compare_links]
         comment_url = compare_linker.add_comment(payload.repo_full_name, payload.pr_number, compare_links)
         comment_url
