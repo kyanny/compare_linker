59f606708adc4f58f097d0e626a56d17ba7b867a
Kensuke Nagae <kyanny@gmail.com>
2014-01-20 02:35:38 +0900

    fix

--- lib/compare_linker/rack_app.rb
+++ lib/compare_linker/rack_app.rb
@@ -12,7 +12,7 @@ class CompareLinker
       payload = CompareLinker::WebhookPayload.new(params["payload"])
       if payload.action == "opened"
         compare_linker = CompareLinker.new(payload.repo_full_name, payload.pr_number)
-        compare_links = compare_linker.make_compare_link.join("\n")
+        compare_links = compare_linker.make_compare_links.join("\n")
         comment_url = compare_linker.add_comment(payload.repo_full_name, payload.pr_number, compare_links)
         comment_url
       end
