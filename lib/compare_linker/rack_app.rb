ec2d66cd708c27601ddc2cd451f01b531d02ac56
Kensuke Nagae <kyanny@gmail.com>
2014-01-20 23:36:15 +0900

    Fix

--- lib/compare_linker/rack_app.rb
+++ lib/compare_linker/rack_app.rb
@@ -38,14 +38,14 @@ class CompareLinker
       pr_number = params["pr_number"]
       logger.info "repo_full_name=#{repo_full_name} pr_number=#{pr_number}"
 
-      compare_linker = CompareLinker.new(payload.repo_full_name, payload.pr_number)
+      compare_linker = CompareLinker.new(repo_full_name, pr_number)
       compare_linker.formatter = CompareLinker::Formatter::Markdown.new
       compare_links = compare_linker.make_compare_links.join("\n")
 
       if compare_links.nil? || compare_links.empty?
         logger.info "no compare links"
       else
-        comment_url = compare_linker.add_comment(payload.repo_full_name, payload.pr_number, compare_links)
+        comment_url = compare_linker.add_comment(repo_full_name, pr_number, compare_links)
         logger.info comment_url
       end
     end
