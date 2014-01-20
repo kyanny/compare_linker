879a4db487b1978071d08f9d3173d9b41ac1ab72
Kensuke Nagae <kyanny@gmail.com>
2014-01-21 03:18:38 +0900

    fix worker

--- lib/compare_linker/iron_worker.rb
+++ lib/compare_linker/iron_worker.rb
@@ -1,10 +1,10 @@
 class CompareLinker
   class IronWorker
-    def self.run
+    def self.run(params)
       puts "Starting CompareLinker::IronWorker at #{Time.now}"
       puts "Payload: #{params}"
 
-      compare_linker = CompareLinker.new(params[:repo_full_name], params[:pr_number])
+      compare_linker = CompareLinker.new(params["repo_full_name"], params["pr_number"])
       compare_linker.formatter = CompareLinker::Formatter::Markdown.new
       compare_links = compare_linker.make_compare_links.join("\n")
 
@@ -20,4 +20,4 @@ class CompareLinker
   end
 end
 
-CompareLinker::IronWorker.run
+CompareLinker::IronWorker.run(params)
