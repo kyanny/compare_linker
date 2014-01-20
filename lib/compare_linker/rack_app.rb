47944d484b4092c4a04388fa764f39c5ccf84fad
Kensuke Nagae <kyanny@gmail.com>
2014-01-20 15:57:23 +0900

    debug

--- lib/compare_linker/rack_app.rb
+++ lib/compare_linker/rack_app.rb
@@ -12,10 +12,16 @@ class CompareLinker
 
     post "/webhook" do
       payload = CompareLinker::WebhookPayload.new(params["payload"])
+      puts 1
+      p [payload.action, payload.repo_full_name, payload.pr_number]
       if payload.action == "opened"
+        puts 2
         compare_linker = CompareLinker.new(payload.repo_full_name, payload.pr_number)
+        puts 3
         compare_linker.formatter = CompareLinker::Formatter::Markdown.new
+        puts 4
         compare_links = compare_linker.make_compare_links.join("\n")
+        puts 5
         puts compare_links
         # if compare_links.nil?
         #   puts "no compare links"
