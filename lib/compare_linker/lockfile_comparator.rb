96e4543e54cd3486ae2f28059118ba5ac9e3ffca
Kensuke Nagae <kyanny@gmail.com>
2014-01-28 16:58:42 +0900

    [bugfix] fix regexp

--- lib/compare_linker/lockfile_comparator.rb
+++ lib/compare_linker/lockfile_comparator.rb
@@ -13,7 +13,7 @@ class CompareLinker
             old_rev = old_spec.source.options["revision"]
             new_rev = new_spec.source.options["revision"]
             if old_rev && new_rev && (old_rev != new_rev)
-              _, owner, gem_name = old_spec.source.uri.match(/github\.com\/(\w+)\/(\w+)/).to_a
+              _, owner, gem_name = old_spec.source.uri.match(/github\.com\/([^\/]+)\/([^.]+)/).to_a
               updated_gems[old_spec.name] = {
                 owner: owner,
                 gem_name: gem_name,
