a4d695f39b5c5e13147792912b593ce8f4f8e46c
Kensuke Nagae <kyanny@gmail.com>
2014-02-07 14:05:09 +0900

    Add more test to cover normal case

--- spec/lib/compare_linker/github_link_finder_spec.rb
+++ spec/lib/compare_linker/github_link_finder_spec.rb
@@ -5,15 +5,28 @@ describe CompareLinker::GithubLinkFinder do
 
   subject { described_class.new(octokit) }
 
-  before do
-    HTTPClient.stub_chain(:get, :body) { load_fixture("web_translate_it.json") }
-  end
 
   describe "#find" do
-    it "extracts repo_owner and repo_name without trailing slash" do
-      subject.find("web_translate_it")
-      expect(subject.repo_owner).to eq "atelierconvivialite"
-      expect(subject.repo_name).to eq "webtranslateit"
+    before do
+      HTTPClient.stub_chain(:get, :body) { load_fixture("rails.json") }
+    end
+
+    it "extracts repo_owner and repo_name" do
+      subject.find("rails")
+      expect(subject.repo_owner).to eq "rails"
+      expect(subject.repo_name).to eq "rails"
+    end
+
+    context "if github url includes trailing slash" do
+      before do
+        HTTPClient.stub_chain(:get, :body) { load_fixture("web_translate_it.json") }
+      end
+
+      it "extracts repo_owner and repo_name without trailing slash" do
+        subject.find("web_translate_it")
+        expect(subject.repo_owner).to eq "atelierconvivialite"
+        expect(subject.repo_name).to eq "webtranslateit"
+      end
     end
   end
 end
