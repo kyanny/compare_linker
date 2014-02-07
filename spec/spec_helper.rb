97f06d616b6a08de8d7bd584522b1703c77f0654
Kensuke Nagae <kyanny@gmail.com>
2014-02-07 14:00:10 +0900

    Add support helper

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,5 +1,11 @@
 require 'rspec'
 require "compare_linker"
 
+
+Dir["#{__dir__}/support/**/*.rb"].each do |f|
+  require f
+end
+
 RSpec.configure do |config|
+  config.include LoadFixtureHelper
 end
