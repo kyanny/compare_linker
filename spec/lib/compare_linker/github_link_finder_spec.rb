09a21120e0b17915c4b5a09449c8cf0f1f7173fb
Kensuke Nagae <kyanny@gmail.com>
2014-02-07 14:00:24 +0900

    Add regression test with failed fixture

--- spec/lib/compare_linker/github_link_finder_spec.rb
+++ spec/lib/compare_linker/github_link_finder_spec.rb
@@ -1,5 +1,19 @@
 require "spec_helper"
 
 describe CompareLinker::GithubLinkFinder do
+  let(:octokit) { double.as_null_object }
 
+  subject { described_class.new(octokit) }
+
+  before do
+    HTTPClient.stub_chain(:get, :body) { load_fixture("web_translate_it.json") }
+  end
+
+  describe "#find" do
+    it "extracts repo_owner and repo_name without trailing slash" do
+      subject.find("web_translate_it")
+      expect(subject.repo_owner).to eq "atelierconvivialite"
+      expect(subject.repo_name).to eq "webtranslateit"
+    end
+  end
 end
