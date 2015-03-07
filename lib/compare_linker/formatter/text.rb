3037be1e91215f6b2c294be66fbd21c99d318cf3
Takashi Masuda <masutaka.net@gmail.com>
2015-03-07 23:16:20 +0900

    Fix wrong guess of downgrade

--- lib/compare_linker/formatter/text.rb
+++ lib/compare_linker/formatter/text.rb
@@ -19,11 +19,22 @@ class CompareLinker
           "#{g.gem_name} (link not found): #{g.old_ver} => #{g.new_ver}"
         end
 
-        if (g.old_tag && g.new_tag && g.new_tag.to_f < g.old_tag.to_f) || g.new_ver.to_f < g.old_ver.to_f
+        if downgrade?(g.old_ver, g.new_ver, g.old_tag, g.new_tag)
           text += " (downgrade)"
         end
 
         text
+      end
+
+      private
+
+      def downgrade?(old_ver, new_ver, old_tag, new_tag)
+        Gem::Version.new(new_ver) < Gem::Version.new(old_ver) ||
+          (old_tag && new_tag && Gem::Version.new(to_ver(new_tag)) < Gem::Version.new(to_ver(old_tag)))
+      end
+
+      def to_ver(tag)
+        tag.sub(/\Av/, '')
       end
     end
   end
